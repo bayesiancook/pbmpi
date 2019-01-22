6fe85183c585ed355e0a419e01678c73a98d831f
bayesiancook <nicolas.lartillot@gmail.com>
2019-01-22 21:06:28 +0100

    fixed MPI issue with gcc 8

--- sources/PB.cpp
+++ sources/PB.cpp
@@ -24,16 +24,6 @@ int main(int argc, char* argv[])	{
 	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 
-	int blockcounts[2] = {1,3};
-	MPI_Datatype types[2] = {MPI_DOUBLE,MPI_INT};
-	MPI_Aint dtex,displacements[2];
-	
-	displacements[0] = (MPI_Aint) 0;
-	MPI_Type_extent(MPI_DOUBLE,&dtex);
-	displacements[1] = dtex;
-	MPI_Type_struct(2,blockcounts,displacements,types,&Propagate_arg);
-	MPI_Type_commit(&Propagate_arg); 
-
 	if (! myid)	{
 		cerr << '\n';
 	}
